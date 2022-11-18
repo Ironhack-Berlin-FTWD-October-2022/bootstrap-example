6233d8a50325f9f0e650b75e185186bc9a6370a9
dmquinn <danielmurphyberling@gmail.com>
2022-11-18 17:05:02 +0100

    friday

--- app.js
+++ app.js
@@ -27,7 +27,14 @@ app.get("/list-group", (req, res) => {
   res.render("list-group");
 });
 app.get("/spinner", (req, res) => {
-  res.render("spinner");
+  const data = [
+    {
+      photo:
+        "https://images.unsplash.com/photo-1526666923127-b2970f64b422?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80",
+    },
+  ];
+
+  res.render("spinner", { data });
 });
 app.get("/movies", (req, res) => {
   const movies = [
